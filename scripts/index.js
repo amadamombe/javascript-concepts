6495ec6b0d7948ab07536aecac31d3f1e66edc1e
Augustine Madamombe <augustine@thedatashed.co.uk>
2021-02-25 21:25:07 +0000

    added code to index.js and index.html file

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+alert("Hello World");
+console.log("Secret message");
+
+document.write("Break time!");
+
+// Task 1
+var myName = "Augustine";
+var myTown = "Leeds";
+
+console.log("My name is " + myName + " and i live in " + myTown);
+
+
+
+// Task 
+var firstNumber = 74.23;
+var secondNumber = 11.20;
+
+var sum = firstNumber + secondNumber;
+var diff = firstNumber - secondNumber;
+var mult = firstNumber * secondNumber;
+var mod = 4 % 3;
+
+console.log(sum);
+console.log(diff);
+console.log(mod);
+console.log(mult.toFixed(2)); // round to 2 decimal places
+
+
+// Tip Task  
+var preTipTotal = 85.50;
+var tipPercentage = 0.1;
+
+var tip = preTipTotal * tipPercentage;
+var totalBill = preTipTotal + tip;
+
+console.log(tip);
+console.log(`Your total bill with tip is ${totalBill}`);
+
+
+
+// Task 
+var fName = "Augustine";
+var lName = " Madamombe";
+var fullName = fName + lName;
+
+console.log(`My name is ${fullName}`);
